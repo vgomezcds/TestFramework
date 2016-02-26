db492f23c37b2ea04a6fd54424d6ecdad04411af
Viry Gomez <vgomez@codigodelsur.com>
2016-02-26 13:15:13 -0300

    update

--- TestFramework.framework/Headers/TestFramework.h
+++ TestFramework.framework/Headers/TestFramework.h
@@ -6,14 +6,4 @@
 //  Copyright © 2016 codigodelsur. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for TestFramework.
-FOUNDATION_EXPORT double TestFrameworkVersionNumber;
-
-//! Project version string for TestFramework.
-FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
-
 #import <TestFramework/FClass.h>
